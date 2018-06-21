4bf85456c934302d05b99aca0e67045ed6f909aa
VTECHHJ\EDE67167 <hy_yan@vtech.com>
2018-06-21 08:50:08 +0800

    V2.8.0,Add katex to img,the code when undefine the language but keep the \n

--- _git.bat
+++ _git.bat
@@ -1,5 +1,5 @@
 git status
 git add -A
-git commit -m "V2.8.0,Add katex to img,the code when undefine the language but keep the \n"
+git commit -m " V2.8.0,Add katex to img,the code when undefine the language but keep the \n"
 git push
 pause
